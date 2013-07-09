ab54bb69f2927ac6dbe84e2daa066a98920179a1
Ubuntu <ubuntu@ip-172-31-33-249.us-west-2.compute.internal>
2013-07-09 20:05:10 +0000

    grader.js

--- grader.js
+++ grader.js
@@ -3,6 +3,7 @@
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
